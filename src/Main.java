dc2c69f3d02828c065c0c05b4c69dfb8c7c53aa0
Naomi <naomi.bala2003@yahoo.com>
2022-10-09 15:15:43 +0300

    Tema Lab 9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello!");
+        System.out.println("Naomi");
+
+        byte ByteVariable = 9 + 13;
+        System.out.println(ByteVariable);
+        ByteVariable = 12 / 2;
+        System.out.println(ByteVariable);
+        ByteVariable = - 5 + 8 * 6;
+        System.out.println(ByteVariable);
+        ByteVariable = ( 55 + 9 ) % 9;
+        System.out.println(ByteVariable);
+        ByteVariable = 20 + - 3 * 5 / 8;
+        System.out.println(ByteVariable);
+        ByteVariable = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(ByteVariable);
+
+
+
+
+
     }
 }
